b5653781bc8a9c80c17d955d999b0fb0ad85dff8
Lee Khang <lekhang2512@gmail.com>
2018-10-11 13:17:56 +0700

    first commit

--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueUploadMultipleImage from './components/VueUploadMultipleImage'
 
-// if (document.querySelector('#my-strictly-unique-vue-upload-multiple-image')) {
-//   Vue.component('VueUploadMultipleImage', VueUploadMultipleImage)
+if (document.querySelector('#my-strictly-unique-vue-upload-multiple-image')) {
+  Vue.component('VueUploadMultipleImage', VueUploadMultipleImage)
 
-//   new Vue({
-//     el: '#my-strictly-unique-vue-upload-multiple-image',
-//     render: h => h(App)
-//   })
-// }
+  new Vue({
+    el: '#my-strictly-unique-vue-upload-multiple-image',
+    render: h => h(App)
+  })
+}
 
 export default VueUploadMultipleImage
