d8c0ab6e5e9065a4fd602098360d89cccaea548c
Supernova <yunlong870924@gmail.com>
2023-07-15 21:34:14 +0800

    tidy up code styles

--- src/App.vue
+++ src/App.vue
@@ -9,38 +9,18 @@
 import VueUploadMultipleImage from './components/VueUploadMultipleImage'
 export default {
   name: 'app',
-  data() {
-    return {
-    }
-  },
   components: {
     VueUploadMultipleImage
   },
   methods: {
-    uploadImageSuccess(formData, index, fileList) {
-      console.log('data', formData, index, fileList)
-      // Upload image api
-      // axios.post('http://gostore.gostore-api.test/api/items/upload', formData).then(response => {
-      //   console.log(response)
-      // })
-    },
+
     beforeRemove(index, done, fileList) {
-      console.log('index', index, fileList)
       var r = confirm("remove image")
       if (r == true) {
         done()
-      } else {
       }
     },
-    editImage(formData, index, fileList) {
-      console.log('edit data', formData, index, fileList)
-    },
-    dataChange(data) {
-      console.log(data)
-    },
-    limitExceeded(amount) {
-      console.log(amount)
-    }
+
   }
 }
 </script>
